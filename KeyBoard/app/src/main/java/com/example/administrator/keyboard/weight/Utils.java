e0d03840a43c707a16615f72d574aa3dcf487e88
Your Name <1184316436@qq.com>
2018-04-14 17:42:21 +0800

    自定义软键盘

---
+++ KeyBoard/app/src/main/java/com/example/administrator/keyboard/weight/Utils.java
@@ -0,0 +1,19 @@
+package com.example.administrator.keyboard.weight;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 2018/4/14.
+ */
+
+public class Utils {
+    public static int dipToPx(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int pxToDip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
